5c8fcde5400ea2b9683434a2ca2b6c979d5ed29e
Ryan Ho <rhohoman@gmail.com>
2024-11-21 13:17:18 -0500

    cleaned up Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,15 +14,6 @@ export const Footer = () => {
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
             <a
-              href="#"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:underline me-4 md:me-6">
-              About
-            </a>
-          </li>
-          <li>
-            <a
               href="https://github.com/Rhohoman"
               target="_blank"
               rel="noreferrer"
